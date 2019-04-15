e98d185086b8d8753bac007be650faf094252e30
Tony Williams <92tonywills@gmail.com>
2019-04-15 22:37:03 +0100

    Added movement and tile spawning

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private Vector3 _velocity;
+
+    private void Update()
+    {
+        transform.position += _velocity * Time.deltaTime;
+    }
+
+}
---
+++ Assets/Scripts/TileSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpawner : MonoBehaviour
+{
+    [Header("Tile Spawning")]
+    [SerializeField] private GameObject _tilePrefab;
+    [SerializeField] private int _initialSpawnCount;
+    [SerializeField] private int _tileWidth;
+    [SerializeField] [Tooltip("Time between tile spawns")] private int _tileSpawnRate;
+
+    private readonly LinkedList<GameObject> _tiles = new LinkedList<GameObject>();
+
+    private int _nextTilePosition;
+    private float _spawnProgress;
+
+    private void Awake()
+    {
+        _spawnProgress = _tileSpawnRate;
+        _nextTilePosition = -_tileWidth;
+        for (int i = 0; i < _initialSpawnCount; i++)
+        {
+            GenerateTile();
+        }
+    }
+
+    private void GenerateTile()
+    {
+        Vector3 position = new Vector3(0, 0, _nextTilePosition);
+        GameObject tile = Instantiate( _tilePrefab, position, Quaternion.identity, transform);
+        _tiles.AddLast(tile);
+        _nextTilePosition += _tileWidth;
+    }
+
+    private void Update()
+    {
+        _spawnProgress -= Time.deltaTime;
+        if (_spawnProgress <= 0)
+        {
+            GenerateTile();
+            Destroy(_tiles.First.Value);
+            _tiles.RemoveFirst();
+            _spawnProgress = _tileSpawnRate;
+        }
+    }
+}
